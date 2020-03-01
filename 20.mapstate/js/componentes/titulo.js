76ffae1aa2b70ae69341f1abc60ade17256437a5
Erickhr <Erick08_999@hotmail.com>
2020-03-01 00:05:07 -0600

    mapstate- titulo

--- 20.mapstate/js/componentes/titulo.js
+++ 20.mapstate/js/componentes/titulo.js
@@ -2,10 +2,13 @@ Vue.component('titulo',{
     template:
     `
     <div>
-        <h1>numero{{$store.state.numero}}</h1> 
+        <h1>numero{{numero}}</h1> 
         <button @click="$store.state.numero++">+</button>
 
         <hijo></hijo>
     </div>
-    `
+    `,
+    computed:{
+        ...Vuex.mapState(['numero'])
+    }
 })
